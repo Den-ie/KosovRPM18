5e755507a5906bf62ede7b7d6dd5b948d55745fa
Den-ie <dpokosov@gmail.com>
2023-04-17 12:43:23 +0300

    Добавьте файлы проекта.

---
+++ KosovRPM18/ModelExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KosovRPM18
+{
+    public partial class AccountingEntites : DbContext
+    {
+        private static AccountingEntites context;
+
+        public static AccountingEntites GetContext()
+        {
+            if (context == null)
+                context = new AccountingEntites();
+            return context;
+        }
+    }
+}
